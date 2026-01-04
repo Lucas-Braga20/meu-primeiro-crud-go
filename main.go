9a94f6a949779cfd7659e14a46d79c051e559ff0
Lucas Braga <lucasfbraga200@gmail.com>
2026-01-04 15:14:12 -0400

    feat: Adiciona requisições de usuário e validações

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"log"
+	"github.com/Lucas-Braga20/meu-primeiro-crud-go/src/controller/routes"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -12,5 +12,12 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	fmt.Println(os.Getenv("TEST"))
+
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ src/controller/createUser.go
@@ -0,0 +1,25 @@
+package controller
+
+import (
+	"fmt"
+
+	"github.com/Lucas-Braga20/meu-primeiro-crud-go/src/configuration/rest_err"
+	"github.com/Lucas-Braga20/meu-primeiro-crud-go/src/controller/model/request"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()),
+		)
+
+		c.JSON(restErr.Code, restErr)
+
+		return
+	}
+
+	fmt.Println(userRequest)
+}
